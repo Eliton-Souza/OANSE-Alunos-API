18f4dccb9c9fc8389ce5beab63e501bc1b4d9604
Eliton Souza <elitonsilva@super.ufam.edu.br>
2023-06-29 09:26:06 -0500

    refatorando controller transacao

--- src/controllers/negociacao/transacaoController.ts
+++ src/controllers/negociacao/transacaoController.ts
@@ -7,44 +7,112 @@ import { Lider } from '../../models/Pessoa/Lider';
 
 export const listarTransacoes = async (req: Request, res: Response) => {
 
-    const transacoes = await Transacao.findAll({
-        include: [
-            {
-              model: Aluno,
-              attributes: [],
-              include: [{
-                model: Pessoa,
-                attributes: ['nome', 'sobrenome'] 
-                }]
-            },
-            {
-              model: Lider,
-              attributes: [],
-              include: [{
+    try {
+        const transacoes = await Transacao.findAll({
+            include: [
+                {
+                model: Aluno,
+                attributes: [],
+                include: [{
                     model: Pessoa,
-                    attributes: ['nome', 'sobrenome'] 
-                }]
-            },
-          ],
-        attributes: ['tipo','valor', 'data', 'novo_saldo', 'id_transacao'],
-        raw: true
-    });
-    res.json({transacoes});
-}
+                    attributes: ['nome'] 
+                    }]
+                },
+                {
+                model: Lider,
+                attributes: [],
+                include: [{
+                        model: Pessoa,
+                        attributes: ['nome'] 
+                    }]
+                },
+            ],
+            attributes: ['tipo','valor', 'id_transacao'],
+            raw: true
+        });
+
+        const transacaoFormatada = transacoes.map((transacao: any) => {
+            return {
+            id_transacao: transacao.id_transacao,
+            tipo: transacao.tipo,
+            valor: transacao.valor,
+            nome_lider: transacao['Lider.Pessoa.nome'],
+            nome_aluno: transacao['Aluno.Pessoa.nome'],
+            };
+        });
+        return res.json({ transacoes: transacaoFormatada });
+      
+    } catch (error) {
+      return res.json({error: "Erro ao encontrar transacoes"});
+    }
+  };
 
 
 export const pegarTransacao = async (req: Request, res: Response) => {
 
     let id= req.params.id;
-    const transacao= await Transacao.findByPk(id);
+    try {
+        const transacaoResponse = await Transacao.findByPk(id, {
+            include: [
+                {
+                model: Aluno,
+                attributes: [],
+                include: [{
+                    model: Pessoa,
+                    attributes: ['nome', 'sobrenome'] 
+                    }]
+                },
+                {
+                model: Lider,
+                attributes: [],
+                include: [{
+                        model: Pessoa,
+                        attributes: ['nome', 'sobrenome'] 
+                    }]
+                },
+            ],
+            attributes: ['tipo','valor', 'data', 'novo_saldo', 'id_transacao'],
+            raw: true
+        });
+        interface transacaoFormatada {
+            id_transacao: number,
+            tipo: string,
+            valor: number,
+            descricao: string,
+            data: Date,
+            novoSaldo: number,
 
-    if(transacao){
-        res.json({transacao});
+            nome_lider: string,
+            sobrenome_lider: string,
+
+            nome_aluno: string,
+            sobrenome_aluno: string
+        }
+    
+        const transacao: any= transacaoResponse;
+  
+      const transacaoFormatada: transacaoFormatada = {
+        
+        id_transacao: transacao.id_transacao,
+        tipo: transacao.tipo,
+        valor: transacao.valor,
+        descricao: transacao.descricao,
+        data: transacao.data,
+        novoSaldo: transacao.novo_saldo,
+
+        nome_lider: transacao['Lider.Pessoa.nome'],
+        sobrenome_lider: transacao['Lider.Pessoa.sobrenome'],
+
+        nome_aluno: transacao['Aluno.Pessoa.nome'],
+        sobrenome_aluno: transacao['Aluno.Pessoa.sobrenome'],
+    };
+      
+      return res.json({ transacao: transacaoFormatada });
+    } catch (error) {
+      return res.json({ error: 'Transação não encontrado'});
     }
-    else{
-        res.json({error: 'Transacao não encontrada'});
-    }
-}
+  };
+  
 
 
 export const editarTransacao = async (req: Request, res: Response) => {
@@ -54,18 +122,18 @@ export const editarTransacao = async (req: Request, res: Response) => {
   try {
     const { descricao } = req.body;
 
-    // Recuperar transacao do banco
-    const transacao = await Transacao.findByPk(id_transacao);
-    if (transacao) { 
-        transacao.descricao= descricao;                         //pode editar apenas a descricao
-        await transacao.save();
-        res.json({ Transacao: transacao});
+    // Recuperar transacoes do banco
+    const transacoes = await Transacao.findByPk(id_transacao);
+    if (transacoes) { 
+        transacoes.descricao= descricao;                         //pode editar apenas a descricao
+        await transacoes.save();
+        return res.json({ Transacao: transacoes});
     }
     else{
-        return res.status(404).json({ error: 'Transacao não encontrada' });
+        return res.json({ error: 'Transacao não encontrada' });
     }
    }catch (error:any) {
-    res.status(500).json({ error: 'Erro ao editar Transacao'});
+    res.json({ error: 'Erro ao editar Transacao'});
   }
 };
 
@@ -75,5 +143,5 @@ export const deletarTransacao = async (req: Request, res: Response) => {
     const id_transacao = req.params.id;
 
     await Transacao.destroy({where:{id_transacao}});
-    res.json({});
+    return res.json({});
 };
