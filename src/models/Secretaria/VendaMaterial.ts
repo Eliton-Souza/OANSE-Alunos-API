81bec72b82bf7c764b00bb3b8f4598abece2939c
Eliton Souza <ess3@icomp.ufam.edu.br>
2023-08-26 13:13:15 -0400

    adicionando campo de valor unitario

--- src/models/Secretaria/VendaMaterial.ts
+++ src/models/Secretaria/VendaMaterial.ts
@@ -9,11 +9,13 @@ export interface VendaMaterialInstace extends Model {
     id_venda: number;
     id_material: number;
     quantidade: number;
+    valor_unit: number;
 }
 
 export const Venda_Material_Ass = sequelize.define<VendaMaterialInstace>('Venda_Material_Ass', {
     id_venda_material: {
-        primaryKey: false,
+        primaryKey: true,
+        autoIncrement: false,
         type: DataTypes.STRING
     },
     id_venda:{
@@ -36,10 +38,16 @@ export const Venda_Material_Ass = sequelize.define<VendaMaterialInstace>('Venda_
         type: DataTypes.INTEGER,
         allowNull: false,
     },
+    valor_unit: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
 }, {
     tableName: 'Venda_Material_Ass',
     timestamps: false
 });
+
+
 
 Venda.belongsToMany(Material, {
     through: Venda_Material_Ass,
