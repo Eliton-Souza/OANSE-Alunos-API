5541d31dae477ae03356d0ef272e6a0cd6194791
Eliton Souza <ess3@icomp.ufam.edu.br>
2023-09-04 10:06:00 -0400

    corrigindo validação da entrada de dados

--- src/schemas/secretariaSchema.ts
+++ src/schemas/secretariaSchema.ts
@@ -3,9 +3,9 @@ import Joi from 'joi';
 function materialSchema(metodo: 'optional' | 'required') {
   const materialValidation = Joi.object({
     nome: Joi.string().regex(/^[a-zA-Z0-9\sçÇáÁàÀâÂãÃéÉèÈêÊíÍìÌîÎóÓòÒôÔõÕúÚùÙûÛ\-\.]+$/)[metodo](),
-    quantidade: Joi.number().integer().min(0).optional().optional(),
-    id_clube: Joi.number().integer().min(1).optional()[metodo](),
-    preco: Joi.number().integer().min(0).optional().optional(),
+    quantidade: Joi.number().integer().min(0).optional(),
+    id_clube: Joi.number().integer().min(1)[metodo](),
+    preco: Joi.number().min(0)[metodo](),
   });
 
   return materialValidation;
@@ -20,12 +20,12 @@ export const updateMaterial = materialSchema("optional");    //atualizar
 export const venda = Joi.object({
   materiais: Joi.array().items(
     Joi.object({
-      id_material: Joi.number().integer().min(0).required(),
+      id_material: Joi.number().integer().min(1).required(),
       quantidade: Joi.number().integer().min(1).required(),
-      valor_unit: Joi.number().positive().required(),
+      valor_unit: Joi.number().min(0).required(),
     })
   ).required(),
-  valor_total: Joi.number().positive().required(),
+  valor_total: Joi.number().positive().min(0).required(),
   descricao: Joi.string().allow(null).max(200).optional(),
   id_aluno: Joi.number().integer().min(0).required(),
 });
